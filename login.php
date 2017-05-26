4aae4171acf36ad3e7ca38660286a0067da8da38
nagaraju417 <nagaraju@pranetor.com>
2017-05-26 17:27:46 +0530

    adding LOGIN file

---
+++ login.php
@@ -0,0 +1,177 @@
+<<<<<<< HEAD
+<?php
+error_reporting(0);
+session_start();
+?>
+
+<!doctype html>
+<html>
+=======
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+>>>>>>> adding db file
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Pranetor Website</title>
+<link rel="icon" href="images/favicon.png" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="css/jquery.bxslider.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/responsive.css" />
+<!--[if lt IE 9]>
+<script src="js/html5shiv.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+<section class="mainWraper">
+<section class="wraper">
+	<div class="quotes">
+    	<ul class="socialIcons">
+            <li><a href="#popup4" data-popup="lightbox" class="findOut quoteIcon">Quote</a></li>
+        	<li><a href="#"><span class="fa fa-facebook"></span></a></li>
+            <li><a href="#"><span class="fa fa-behance"></span></a></li>
+        </ul>
+    </div>
+    
+    <header class="header">
+	<div class="container">
+          <div class="row">
+              <div class="col-lg-4 col-md-4 col-sm-4"><h1 id="logo"><a href="index.html"><img src="images/logo.png" alt="logo" /></a></h1></div>
+              <div class="col-lg-8 col-md-8 col-sm-8">
+              	<nav class="navMain">
+              		<ul>
+                    	<li><a href="index.html">Home</a></li>
+                        <li><a href="services.html">Services</a></li>
+                        <li><a href="works.html">Work</a></li>
+                        <li><a href="career.html">Career</a></li>
+                        <li><a href="aboutUs.html">About Us</a></li>
+                        <li><a href="contact.html" class="active">Contact Us</a></li>
+                    </ul>
+                </nav>
+              </div>
+              </div>
+          </div>
+        </header>
+    </div>
+</section>
+
+<div class="lightBox" id="popup4">
+    <div class="lightBoxInner">
+        <div class="lightBoxInner2">
+        <div class="lightBoxContent">
+        	<a class="close" href="#"><span class="fa fa-times"></span></a>
+            <div class="titleBlock">
+                <h3>GET A FREE ESTIMATE</h3>
+            </div>
+            <div class="formBlock">
+            <div class="loading"></div>
+<div class="mess_info"></div>
+       <form id="email" name="form" class="email" method="post">
+        <fieldset>
+          <div class="formRow">
+            <input type="text" name="name" class="inputtab" value="Name" required>
+            <input type="tel" name="phone" class="inputtab" value="Phone" required>
+            <input type="email" name="email" class="inputtab" value="Email" required>
+            <input type="Subject" name="subject" class="inputtab" value="subject" required>
+          </div>
+          
+          <div class="formRow">
+          	 <textarea class="inputtexteria" placeholder="message" name="message" rows="6" cols="6" required></textarea>
+          	 <input type="submit" name="submit" class="subBtn" value="submit">
+          </div>
+
+          
+        </fieldset>
+      </form>
+    </div>
+    </div>
+        </div>
+    </div>
+</div>
+
+
+<section class="contacteMain">
+	<div class="container">
+    	<div class="row">
+        	
+            <div class="contactinnerBg">
+            	<h4>Contact Us</h4>
+            	
+                
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                	<div class="contactMapBg">
+                        
+                        <div class="contacts">
+                              <h6>Address :</h6>
+                              <span>Pranetor</span>
+                              <small>Plot No 17-24, <br/>Reliance Cyber Villa,<br/> Vittal Rao Nagar <br/> Madhapur, Hyderabad<br /> Telangana 500081</small>
+                       </div>
+                        <div class="pranetorAddress">
+                            	<p>+9140-65550441</p>
+                            	<a>Info@pranetor.com</a>
+                        </div> 
+                        
+                        <div class="contactMp">
+                        	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.339650070345!2d78.38303121544315!3d17.443450188044963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb915f9bb7ff87%3A0x71cf5035ab0b77f5!2sreliance+cyber+ville!5e0!3m2!1sen!2sin!4v1484820983272" width="540" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        </div>   
+                            
+                    </div>
+                </div>
+                
+                
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                	<div class="formBlock contectformBlock">
+                	<h3>Request Form</h3>
+ 
+                 <form   name="form"   method="post" action="contact.php">
+                  <fieldset>
+                    <div class="formRow">
+                      <input type="text" name="name" class="texttab" placeholder="Name" required>
+                     </div>
+                     
+                    
+                    <div class="formRow">
+                      <input type="number" name="phone" class="texttab" placeholder="Phone" required>
+                     </div>
+                     
+                     <div class="formRow">
+                      <input type="email" name="email" class="texttab" placeholder="Email" required>
+                    </div>
+                    
+                    <textarea cols="10" rows="10" class="texteritab" placeholder="message" name="message" required></textarea>
+                    <div class="submitbg">
+                      <input type="submit" name="submit" class="submittab" value="submit">
+                    </div>
+                  </fieldset>
+                </form>
+             
+            </div>	
+        
+        
+        </div>
+    </div>
+</section>
+</section>
+
+
+
+
+
+
+
+<footer>
+	<p>© 2015-2016 By Pranetor</p>
+</footer>
+
+<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+
+ </body>
+</html>
